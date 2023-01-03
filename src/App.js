aaf374bd09bf3c657dcb0588fa6911fe155f5685
Sangee-R <sangeethar0205@gmail.com>
2023-01-03 22:28:20 +0530

    SandT

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import { useState } from 'react';
 import Navbar from './component/Navbar';
 import AddStudent from './component/AddStudent';
+import AddMentor from './component/AddMentor';
 import DisplayStudent from './component/DisplayStudent';
+import DisplayMentor from './component/DisplayMentor';
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,6 +14,7 @@ import {
 import Cookies from 'universal-cookie';
 import Home from './component/Home';
 import EditStudent from './component/EditStudent';
+import EditMentor from './component/EditMentor';
  
 const cookies = new Cookies();
  
@@ -45,6 +48,17 @@ function App() {
     cookies.set('data', prev, { path: '/' });
     showAlert('Student added successfully', 'success');
   }
+
+
+  const [mentors, setMentors] = useState(c);
+  const newMentor = (mentor) => {
+    let prev = mentors;
+    prev.push(mentor);
+    setMentors(prev);
+    cookies.remove('data', { path: '/' });
+    cookies.set('data', prev, { path: '/' });
+    showAlert('Mentor added successfully', 'success');
+  }
   const deleteStudent = (id) => {
     let check = window.confirm('Are you sure you want to delete this student?');
     if (check === true) {
@@ -58,6 +72,21 @@ function App() {
       showAlert('Student not deleted', 'warning');
     }
   }
+
+
+  const deleteMentor = (id) => {
+    let check = window.confirm('Are you sure you want to delete this mentor?');
+    if (check === true) {
+      let prev = mentors;
+      prev.splice(id, 1);
+      setMentors(prev);
+      cookies.remove('data', { path: '/' });
+      cookies.set('data', prev, { path: '/' });
+      showAlert('success', 'Mentor deleted successfully');
+    } else {
+      showAlert('Mentor not deleted', 'warning');
+    }
+  }
   
 
   return (
@@ -67,9 +96,15 @@ function App() {
     <Alert alert={alert} />
       <Routes>
         <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/add" element={<AddStudent showAlert={showAlert} newStudent={newStudent}/>}/>
-        <Route path="/display" element={<DisplayStudent showAlert={showAlert} deleteStudent={deleteStudent} students={students} />}/>
+        <Route exact path="/add-Student" element={<AddStudent showAlert={showAlert} newStudent={newStudent}/>}/>
+        <Route path="/display-student" element={<DisplayStudent showAlert={showAlert} deleteStudent={deleteStudent} students={students} />}/>
         <Route path="/edit/:id" element={<EditStudent students={students} showAlert={showAlert} setStudents={setStudents}/>} />
+
+
+
+        <Route exact path="/add-mentor" element={<AddMentor showAlert={showAlert} newMentor={newMentor}/>}/>
+        <Route exact path="/display-mentor" element={<DisplayMentor showAlert={showAlert} deleteMentor={deleteMentor} mentors={mentors}/>}/>
+        <Route exact path="/edit-mentor/:id" element={<EditMentor mentors={mentors} showAlert={showAlert} setMentors={setMentors}/>} />
       </Routes>
     </Router>
     </>
